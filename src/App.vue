6ef292af48560a1151fecfe41baeb1b32d948bd0
tazaq <tazaqsp@mail.ru>
2020-07-30 02:22:21 +0500

    Добавлен компонент для адреса

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-    <h1 class="align-center">Новый клиент</h1>
+      <div class="app-wrapper">
+              <h1 class="align-center">Новый клиент</h1>
 
-    <div class="form-container">
-      <P1_User></P1_User>
-    </div>
-
+          <div class="form-container">
+              <P1_User></P1_User>
+              <P1_UserAddress></P1_UserAddress>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 import P1_User from "@/components/P1_User";
+import P1_UserAddress from "@/components/P1_UserAddress";
 
 export default {
   name: 'App',
   components: {
-    P1_User
+    P1_User,
+    P1_UserAddress
   },
 }
 </script>
---
+++ src/components/P1_UserAddress.vue
@@ -0,0 +1,50 @@
+<template>
+    <fieldset class="form-container-item">
+        <legend>Адрес</legend>
+
+        <label>
+            <span>Индекс</span>
+            <input type="text" v-model="postalCode">
+        </label>
+
+        <label>
+            <span>Страна</span>
+            <input type="text" v-model="postalCode">
+        </label>
+
+        <label>
+            <span>Область</span>
+            <input type="text" v-model="postalCode">
+        </label>
+
+        <label>
+            <span>Город<em>*</em></span>
+            <input type="text" v-model="postalCode" required>
+        </label>
+
+        <label>
+            <span>Улица</span>
+            <input type="text" v-model="postalCode">
+        </label>
+
+        <label>
+            <span>Дом</span>
+            <input type="text" v-model="postalCode">
+        </label>
+    </fieldset>
+</template>
+
+
+<script>
+    export default {
+        name: 'P1_UserAddress',
+        props: {
+            postalCode: String,
+            country: String,
+            region: String,
+            city: String,
+            street: String,
+            house: String
+        }
+    }
+</script>
