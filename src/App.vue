6fae87bc10cde584645914b7280ebdbeb25a0566
tazaq <tazaqsp@mail.ru>
2020-07-30 01:45:40 +0500

    Инициализация

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1 class="align-center">Новый клиент</h1>
+
+    <div class="form-container">
+      <P1_User></P1_User>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import P1_User from "@/components/P1_User";
+
+export default {
+  name: 'App',
+  components: {
+    P1_User
+  },
+}
+</script>
---
+++ src/components/P1_User.vue
@@ -0,0 +1,102 @@
+<template>
+    <fieldset class="form-container-item">
+        <legend>Информация о пользователе</legend>
+
+        <label>
+            <span>Фамилия<em>*</em></span>
+            <input type="text" v-model="lastName" required>
+        </label>
+
+        <label>
+            <span>Имя<em>*</em></span>
+            <input type="text" :firstName="firstName" required>
+        </label>
+
+        <label>
+            <span>Отчество</span>
+            <input type="text" :middleName="middleName">
+        </label>
+
+        <label>
+            <span>Дата рождения</span>
+            <input type="date" :dob="dob">
+        </label>
+
+        <label>
+            <span>Телефон<em>*</em></span>
+            <input type="number" v-bind:phone="phone" maxlength="11">
+        </label>
+
+        <label>
+            <span>Пол</span>
+            <select v-model="genderModel">
+                <option disabled>Выберите один из вариантов</option>
+                <option v-for="(item, index) in genders" v-bind:value="index" :key="index">
+                    {{ item }}
+                </option>
+            </select>
+        </label>
+
+        <label>
+            <span>Группа клиентов</span>
+            <select v-model="clientsGroupModel" multiple>
+                <option disabled>Выберите один или несколько вариантов</option>
+                <option v-for="(clGroup, index) in clientsGroups" v-bind:value="index" :key="index">
+                    {{ clGroup }}
+                </option>
+            </select>
+        </label>
+
+        <label>
+            <span>Лечащий врач</span>
+            <select v-model="doctorModel">
+                <option disabled>Выберите один из вариантов</option>
+                <option v-for="(item, index) in doctors" v-bind:value="index" :key="index">
+                    {{ item }}
+                </option>
+            </select>
+        </label>
+
+        <label>
+            <div>
+                <input type="checkbox" v-model="isDontSendSMS" id="isDontSendSMS"><span>Не отправлять СМС</span>
+            </div>
+        </label>
+
+    </fieldset>
+</template>
+
+
+
+<script>
+    let default_genders = ['Женский', 'Мужской', 'Не указан'];
+    let default_clients_groups = ['VIP', 'Проблемные', 'ОМС'];
+    let default_doctors = ['Иванов', 'Захаров', 'Чернышева'];
+
+    export default {
+        name: 'P1_User',
+        props: {
+            lastName: String,
+            firstName: String,
+            middleName: String,
+            dob: Date,
+            phone: String,
+            genders: Array,
+            genderModel: Number || null,
+            clientsGroups: Array,
+            clientsGroupModel: Array || null,
+            doctors: Array,
+            doctorModel: Number || null,
+            isDontSendSMS: Boolean
+        },
+        created: function () {
+            this.genderModel = null;
+            this.clientsGroupModel = [];
+            this.doctorModel = null;
+
+            this.genders = default_genders;
+            this.clientsGroups = default_clients_groups;
+            this.doctors = default_doctors;
+        }
+    }
+</script>
